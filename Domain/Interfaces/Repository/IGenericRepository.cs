b6fa37a4d7e7acd7249b541d52849a85c592c22f
Fernando Cesar <juniorlemosoi@gmail.com>
2024-02-19 00:35:08 -0300

    Add project files.

---
+++ Domain/Interfaces/Repository/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Repository
+{
+    public interface IGenericRepository <T> where T : BaseEntity
+    {
+        Task<T> InsertAsync(T entity);
+
+        Task<T> UpdateAsync(T entity);
+
+        Task<T> DeleteAsync(int id);
+
+        Task<T> GetByIdAsync(int id);
+
+        Task<IEnumerable<T>> GetAllAsync();
+    }
+}
